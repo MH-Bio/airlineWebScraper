d12bd49ea6fa0cb097ebe6f0522b21ddcc767f42
Michael C. Hernandez <michaelhern@hotmail.com>
2023-05-28 22:58:58 -0700

    Started implimentation of the date selection from the calendar. Requires a lot of navigating through arrays.

--- dateManagement.py
+++ dateManagement.py
@@ -1,14 +1,28 @@
+import calendar
+import numpy as np
+from datetime import datetime
+calendar.setfirstweekday(6)
+
 class departureDate:
     def __init__(self, year, month, day):
-        self.year = -1
-        self.month = -1
-        self.day = -1
+        self.year = year
+        self.month = month
+        self.day = day
 
 class returnDate:
     def __init__(self, year, month, day):
-        self.year = -1
-        self.month = -1
-        self.day = -1
+        self.year = year
+        self.month = month
+        self.day = day
+
+class currentTime:
+    def __init__(self, currentSecond, currentMinute, currentHour, currentDay, currentMonth, currentYear):
+        self.currentSecond = datetime.now().second
+        self.currentMinute = datetime.now().minute
+        self.currentHour = datetime.now().hour
+        self.currentDay = datetime.now().day
+        self.currentMonth = datetime.now().month
+        self.currentYear = datetime.now().year
 
 def getMonthStrShort(monthNum):
     """Returns the three letter code used by United Airlines to input dates"""
@@ -65,3 +79,17 @@ def setDepartureDate(self, year, month, day):
 def setReturnDate(self, year, month, day):
     """Sets the deparutre date global variable"""
     returnDate = returnDate(year, month, day)
+
+def get_week_of_month(year, month, day):
+    """Takes a date as an input and returns the week of the month that day is on (1-6)"""
+    x = np.array(calendar.monthcalendar(year, month))
+    week_of_month = np.where(x == day)[0][0] + 1
+
+    return week_of_month
+
+def get_day_of_week(year, month, day):
+    """Takes a date as an input and returns the day of the week as a number (1-7)"""
+    x = np.array(calendar.monthcalendar(year, month))
+    day_of_week = np.where(x == day)[1][0] + 1
+
+    return day_of_week
--- main.py
+++ main.py
@@ -21,30 +21,75 @@ def unitedAirlines(originCode, destinationCode):
     """Gets info from the United Airlines Website"""
     driver.get(airlineInfo.AIRLINE_WEBSITES['United'])
 
-    depart = dateManagement.departureDate(2023, 8, 1)
+    _depart_obj = dateManagement.departureDate(2023, 8, 1)
+    _return_obj = dateManagement.returnDate(2023, 8, 7)
 
     departDate = driver.find_element(By.ID, 'DepartDate')
     returnDate = driver.find_elements(By.ID, 'ReturnDate')
-    #departStr = '%s %s' % (dateManagement.getMonthStrShort(depart.month), dateManagement.getDayStr(depart.day))
+    #departStr = '%s %s' % (dateManagement.getMonthStrShort(_depart_obj.month), dateManagement.getDayStr(_depart_obj.day))
     #departDate.send_keys(departStr)
-    #departDate.click()
+    departDate.click()
     #departDate.clear()
     #departDate.send_keys('Aug 20')
     #returnDate.clear()
     #returnDate.send_keys('Aug 27')
 
-    try:
-        departDate = driver.find_element(By.CLASS_NAME, 'DateInput_input DateInput_input_1')
-    except:
+    # asdfDate = driver.find_element(By.ID, 'passengersSlidingInputContainer')
 
-        print('ffo')
-        import pdb
-        pdb.set_trace()
+    # Example here: https://learn-automation.com/handle-calender-in-selenium-webdriver/
+    # finding elements in the calnedar: zzz = driver.find_elements(By.XPATH, '//*[@id="passengersSlidingInputContainer"]//td')
 
-    asdfDate = driver.find_element(By.ID, 'passengersSlidingInputContainer')
+    '''
+    Here is an example of an xpath on the united website
+    
+    							                        STATIC                STATIC	    STATIC STATIC STATIC	L/R		   WEEK OF MONTH  DAY OF WEEK
+    //*[@id="passengersSlidingInputContainer"]/div[1]/div/div/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/table/tbody/tr[5]/td[1]
+    
+    or possibly this
+    /html/body/div[19]/div/div/div/div[1]/div[2]/div[2]/div/div[3]/div/table/tbody/tr[3]/td[1] where the last two indicies are week of month and day of week (assuming our month is at the top of the list)
+    
+    In this case L/R refers to the left or right side of the calendar
+    The week of the month is the 1st - 5th week
+    
+    Algorthim for calculating xpath
+    1. calculate out how many months in advance the date is
+    2. click the right button that many time, the correct month should now be in [2] in the L/R index
+    
+    3. Calculate the day of the week, place this in the final index
+    
+    4. Calculate the week of the month.
+        -Take the day of the week and set this as week 1
+        -Subtract the 7 from the current day until your result is less than or equal to 0
+            -The total number of times until you hit the exception is the value for week of the month
+    
+    '''
 
+    current_dateTime = dateManagement.currentTime(0,0,0,0,0,0)
+    difference_between_months = _depart_obj.month - current_dateTime.currentMonth
+
+    next_month_button = driver.find_elements(By.XPATH, '/html/body/div[14]/div/div/div/div[1]/div[2]/div[1]/button[2]')
+
+    i = 1 # remember we are counting the current month as month 1
+    while i < difference_between_months:
+        time.sleep(random.uniform(3, 5))
+        next_month_button[0].click() # advance one month
+        i = i + 1
+
+    united_date_calculation_base = '/html/body/div[19]/div/div/div/div[1]/div[2]/div[2]/div/div[3]/div/table/tbody/tr[%s]/td[%s]'
+
+    depart_week_index_XPathIndex = dateManagement.get_week_of_month(year=_depart_obj.year, month=_depart_obj.month, day=_depart_obj.day)
+    depart_weekday_index_XPathIndex = dateManagement.get_day_of_week(year=_depart_obj.year, month=_depart_obj.month, day=_depart_obj.day)
+
+    return_week_index_XPathIndex = dateManagement.get_week_of_month(year=_return_obj.year, month=_return_obj.month, day=_return_obj.day)
+    return_weekday_index_XPathIndex = dateManagement.get_day_of_week(year=_return_obj.year, month=_return_obj.month, day=_return_obj.day)
+
+    depart_calendar_selection = united_date_calculation_base %(depart_week_index_XPathIndex, depart_weekday_index_XPathIndex)
+    return_calendar_selection = united_date_calculation_base %(return_week_index_XPathIndex, return_weekday_index_XPathIndex)
     import pdb
     pdb.set_trace()
+
+    depart_date_button = driver.find_elements(By.XPATH, depart_calendar_selection)
+
 
     destinationElement = driver.find_element(By.ID, 'bookFlightDestinationInput')
     destinationElement.send_keys(destinationCode)
@@ -61,10 +106,6 @@ def unitedAirlines(originCode, destinationCode):
     time.sleep(random.uniform(3, 5))
     originElement.send_keys(Keys.DOWN)
     originElement.send_keys(Keys.RETURN)
-
-
-
-
 
     submitButton = driver.find_element(By.ID, 'bookFlightForm')
     #submitButton.submit()
